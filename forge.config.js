1a1417b17c061307afbc32249218b96497958c6d
Youritch <86657843+Youritch@users.noreply.github.com>
2023-09-06 21:29:33 +0200

    test export (rip)

--- forge.config.js
+++ forge.config.js
@@ -4,26 +4,34 @@ module.exports = {
 	},
 	rebuildConfig: {},
 	makers: [
-		{
-			name: "@electron-forge/maker-squirrel",
-			config: {},
-		},
-		{
+		/*{
 			name: "@electron-forge/maker-zip",
-			platforms: ["darwin"],
+			platforms: ["darwin", "linux"],
+			config: {
+				// Config here
+			}
 		},
 		{
-			name: "@electron-forge/maker-deb",
+			name: '@electron-forge/maker-deb',
 			config: {
 				options: {
+					maintainer: 'Youritch',
+					homepage: 'https://github.com/Youritch/numworks_desktop'
 					icon: "./Users/youri/Documents/GitHub/numworks_desktop/imgs/icon.png"
 				}
-			},
+			}
 		},
 		{
-			name: "@electron-forge/maker-rpm",
-			config: {},
-		},
-	],
+			name: '@electron-forge/maker-squirrel',
+			config: {
+				icon: "./Users/youri/Documents/GitHub/numworks_desktop/imgs/icon.png"
+				//certificateFile: './cert.pfx',
+				//certificatePassword: process.env.CERTIFICATE_PASSWORD
+			}
+		},*/
+		{
+			name: '@electron-forge/maker-zip'
+		}
+	]
 	
 };
